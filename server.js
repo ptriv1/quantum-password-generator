3a0eb0af7b31815a416d53aea61359d4fb3c9322
PT <trivedp18@gmail.com>
2025-03-25 14:02:49 -0400

    changes to server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+    res.send('Hello world');
+});
+
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
