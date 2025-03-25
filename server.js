dff78cd1fab8e057039018538874c535d412b7ac
PT <trivedp18@gmail.com>
2025-03-25 15:41:12 -0400

    to add quantum-password route

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const app = express();
 
 app.get('/hello', (req, res) => {
     res.send('Hello world');
+});
+
+app.get('/quantum-password', async (req, res) => {
+    const response = await fetch('https://qrng.anu.edu.au/API/jsonI.php?length=8&type=uint8');
+    const data = await response.json();
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
+    const password = data.data.map(number => chars[number % chars.length]).join('');
+    res.send(`Quantum password: ${password}`);
+
 });
 
 app.listen(3000, () => {
